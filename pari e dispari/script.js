85a6e7104afa2b37b44a3f8f8859480d8ed5d0dd
DarioEsposito89 <darioesposito.work@gmail.com>
2023-07-11 12:11:35 +0200

    inserito console.log per somma

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -15,7 +15,7 @@ let userNum = parseInt(prompt("scrivi un numero da 1 a 5"));
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 
 function randomPcNum(){
-   let pcNum = Math.floor(Math.random() * 5) + 1;
+   let pcNum = Math.floor(Math.random() * 5) +1;
     return pcNum;
 }
 
@@ -23,7 +23,9 @@ let randNum = randomPcNum();
 
 // Sommiamo i due numeri
 
-let sum = randomPcNum() + userNum;
+let sum = randNum + userNum;
+console.log (`La somma è ${sum}`)
+
 
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 
